961bc7db48f22ac191a2b9d0ac364acc5f6aa685
Yannis Weishaupt <yannis.weishaupt@gmail.com>
2017-01-26 17:30:05 +0100

    rename chain functions

--- intelliji/src/Class/Chain.java
+++ intelliji/src/Class/Chain.java
@@ -3,13 +3,13 @@ package Class;
 /*Suppose que la lecture du fichier a déjà été faite et que les join+ complément ont déjà été fait*/
 public class Chain
 {
-    /** length, check if the string chain is a multiple of 3*/
-    public static boolean length(String chain)
+    /** isLengthMultipleOf3, check if the string chain is a multiple of 3*/
+    public static boolean isLengthMultipleOf3(String chain)
     {
         return chain.length() % 3 == 0;
     }
     /** charvalid, check if the char is only {A,C,G,T} **/
-    public static boolean chainValid(String chain)
+    public static boolean isChainValid(String chain)
     {
         return chain.toUpperCase().matches("[ATCG]+");
     }
--- intelliji/src/test/ChainTest.java
+++ intelliji/src/test/ChainTest.java
@@ -1,5 +1,4 @@
 import Class.Chain;
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,22 +7,22 @@ public class ChainTest {
     @Test
     public void testLength() throws Exception
     {
-        Assert.assertTrue(Chain.length("abc"));
-        Assert.assertFalse(Chain.length("abcd"));
-        Assert.assertFalse(Chain.length("a"));
-        Assert.assertFalse(Chain.length("ab"));
-        Assert.assertTrue(Chain.length("abcdef"));
-        Assert.assertTrue(Chain.length("abcdefghi"));
-        Assert.assertFalse(Chain.length("abcdefghij"));
+        Assert.assertTrue(Chain.isLengthMultipleOf3("abc"));
+        Assert.assertFalse(Chain.isLengthMultipleOf3("abcd"));
+        Assert.assertFalse(Chain.isLengthMultipleOf3("a"));
+        Assert.assertFalse(Chain.isLengthMultipleOf3("ab"));
+        Assert.assertTrue(Chain.isLengthMultipleOf3("abcdef"));
+        Assert.assertTrue(Chain.isLengthMultipleOf3("abcdefghi"));
+        Assert.assertFalse(Chain.isLengthMultipleOf3("abcdefghij"));
     }
     @Test
     public void testchain() throws  Exception
     {
-        Assert.assertTrue(Chain.chainValid("ATC"));
-        Assert.assertFalse(Chain.chainValid("UTC"));
-        Assert.assertFalse(Chain.chainValid("UVC"));
-        Assert.assertFalse(Chain.chainValid("Uxy"));
-        Assert.assertTrue(Chain.chainValid("agt"));
+        Assert.assertTrue(Chain.isChainValid("ATC"));
+        Assert.assertFalse(Chain.isChainValid("UTC"));
+        Assert.assertFalse(Chain.isChainValid("UVC"));
+        Assert.assertFalse(Chain.isChainValid("Uxy"));
+        Assert.assertTrue(Chain.isChainValid("agt"));
 
 
     }
