9495b89f2b8bf95f0165ed6d7b79565917756347
Yannis Weishaupt <yannis.weishaupt@gmail.com>
2017-01-28 14:09:22 +0100

    Add Tree and TreeTest

--- intelliji/src/test/TreeTest.java
+++ intelliji/src/test/TreeTest.java
@@ -1,25 +1,32 @@
+import Class.Chain;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
 import tree.Tree;
 
-public class Main {
+/**
+ * Created by yannis on 28/01/17.
+ */
+public class TreeTest {
+    private Tree tree;
 
-
-    public static void main(String[] args) {
-
-        // Organism
-        Tree<Tree> blastocatelliaTree= new Tree();
+    @Before
+    public void setup() {
+        // Organism (level 4)
+        Tree<String> blastocatelliaTree= new Tree();
         blastocatelliaTree.add("Chloracidobacterium thermophilum", null);
         blastocatelliaTree.add("Chloracidobacterium thermophilum", null);
         blastocatelliaTree.add("Pyrinomonas methylaliphatogenes", null);
 
-        Tree<Tree> holophagaeTree= new Tree();
+        Tree<String> holophagaeTree= new Tree();
         holophagaeTree.add("Geothrix fermentans", null);
         holophagaeTree.add("Holophaga foetida", null);
 
-        Tree<Tree> solibacteresTree= new Tree();
+        Tree<String> solibacteresTree= new Tree();
         solibacteresTree.add("Bryobacter aggregatus", null);
         solibacteresTree.add("Candidatus Solibacter usitatus ", null);
 
-        // Subgroup
+        // Subgroup (level 3)
         Tree<Tree> acidobacteriaTree= new Tree();
         acidobacteriaTree.add("Acidobacteriia", null);
         acidobacteriaTree.add("Blastocatellia", blastocatelliaTree);
@@ -27,19 +34,31 @@ public class Main {
         acidobacteriaTree.add("Solibacteres", solibacteresTree);
         acidobacteriaTree.add("unclassified Acidobacteria", null);
 
-        // Group
+        // Group (level 2)
         Tree<Tree> bacteriaTree= new Tree();
         bacteriaTree.add("Acidobacteria", acidobacteriaTree);
         bacteriaTree.add("Aquificae", null);
         bacteriaTree.add("Caldiserica", null);
 
-        // Kingdom
-        Tree kingdomTree = new Tree();
+        // Kingdom (level 1)
+        Tree<Tree> kingdomTree = new Tree();
         kingdomTree.add("Archaea", null);
         kingdomTree.add("Bacteria", bacteriaTree);
         kingdomTree.add("Eukaryota", null);
         kingdomTree.add("Viruses", null);
 
-        kingdomTree.printTree();
+        this.tree = kingdomTree;
+    }
+
+    @Test
+    public void testGet() throws Exception
+    {
+
+        Object[] nodes = ((Tree<?>) ((Tree<?>) (((Tree<?>)
+                tree.get("Bacteria"))
+                .get("Acidobacteria")))
+                .get("Holophagae")).nodes();
+        Assert.assertEquals("Geothrix fermentans", nodes[0]);
+        Assert.assertEquals("Holophaga foetida", nodes[1]);
     }
 }
---
+++ intelliji/src/tree/Tree.java
@@ -0,0 +1,85 @@
+package tree;
+
+import java.util.HashMap;
+// import java.util.List;
+
+/**
+ * Created by yannis on 28/01/17.
+ */
+public class Tree<T> {
+
+    private HashMap<String, T> nodes;
+    int size = -1;
+
+    public Tree(){
+        this.nodes = new HashMap<String, T>();
+    }
+
+    public boolean contains(String node){
+        return this.nodes.containsKey(node);
+    }
+
+    public boolean add(String node, T obj){
+        if(! this.contains(node)){
+            this.nodes.put(node, obj);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public T get(String node){
+        return this.nodes.get(node);
+    }
+
+    public Object[] nodes(){
+        return this.nodes.keySet().toArray();
+    }
+
+    public void printTree_aux(int level){
+        if (this != null){
+            Object[] nodess=this.nodes();
+            for (Object node : nodess){
+                for (int i=0; i < level; i++){
+                    System.out.print("-");
+                }
+                System.out.println(node);
+
+                if (this.get((String)node) !=null){
+                    ((Tree<?>) this.get((String)node)).printTree_aux(level+1);
+                }
+            }
+        }
+    }
+
+    public void printTree(){
+        printTree_aux(0);
+    }
+
+    public int computeSize(){
+        int size = 0;
+        for(Object a : this.nodes()){
+            String node = (String)a;
+            if(this.isLeaf(node)){
+                size ++;
+            } else {
+                Tree t = (Tree)this.get(node);
+                size += t.size();
+            }
+        }
+        this.size = size;
+        return size;
+    }
+
+    public int size(){
+        if(this.size == -1){
+            this.computeSize();
+        }
+
+        return this.size;
+    }
+
+    public boolean isLeaf(String node) {
+        return ! (this.nodes.get(node) instanceof Tree);
+    }
+}
