b55f1b899b44798799c04e1f238205033cd60955
Saetlan <s.arnaud67@hotmail.com>
2017-02-02 11:52:53 +0100

    Creating basic Frame

--- intelliji/src/Main.java
+++ intelliji/src/Main.java
@@ -2,14 +2,25 @@ import config.ConfigManager;
 import config.ProductionConfig;
 import tree.OrganismTree;
 import tree.Tree;
+import view.MainFrame;
+
+import javax.swing.*;
 
 public class Main {
 
 
     public static void main(String[] args) {
+
+        JFrame jfMain = new MainFrame();
+        jfMain.setVisible(true);
+
+
         ConfigManager.setConfig(new ProductionConfig());
 
         OrganismTree tree = OrganismTree.fromGenBank();
         tree.printTree();
+
+
+
     }
 }
---
+++ intelliji/src/view/MainFrame.java
@@ -0,0 +1,53 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by arnaudSteinmetz on 29/01/2017.
+ **/
+public class MainFrame extends JFrame {
+
+    private JPanel jpnWest;
+    private JPanel jpnEast;
+    private JPanel jpnNorth;
+    private JPanel jpnSouth;
+
+
+    public MainFrame() {
+        this.setSize(700,500);
+
+        jpnWest = new JPanel();
+        jpnEast = new JPanel();
+        jpnNorth = new JPanel();
+        jpnSouth = new JPanel();
+
+
+        JScrollPane jspMain = new JScrollPane();
+        JTree jtMain = new JTree();
+
+        jspMain.add(jtMain);
+        jpnWest.add(jspMain);
+
+
+        jpnWest.setBackground(Color.BLUE);
+        jpnEast.setBackground(Color.BLUE);
+        jpnNorth.setBackground(Color.BLUE);
+        jpnSouth.setBackground(Color.BLUE);
+
+        this.add(jpnWest, BorderLayout.WEST);
+        this.add(jpnEast, BorderLayout.EAST);
+        this.add(jpnNorth, BorderLayout.NORTH);
+        this.add(jpnSouth, BorderLayout.SOUTH);
+
+
+
+
+
+
+
+
+    }
+
+
+}
